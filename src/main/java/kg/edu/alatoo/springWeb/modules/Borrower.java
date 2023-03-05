3df0c6722197ffa5b558be2ec3acd715ca089cd5
Gulnaz Gimaletdinova <gulnaz1024@yandex.ru>
2023-03-05 16:07:09 +0600

    add Borrower

---
+++ src/main/java/kg/edu/alatoo/springWeb/modules/Borrower.java
@@ -0,0 +1,79 @@
+package kg.edu.alatoo.springWeb.modules;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Borrower {
+
+    @Id @GeneratedValue
+    private long id;
+    private String name;
+    private String email;
+    private String phone_number;
+
+
+    public Borrower() {
+    }
+
+    public Borrower(String name, String email, String phone_number) {
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Borrower setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Borrower borrower = (Borrower) o;
+        return id == borrower.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+
+
+}
+
---
+++ src/main/java/kg/edu/alatoo/springWeb/repos/BorrowerRepository.java
@@ -0,0 +1,11 @@
+package kg.edu.alatoo.springWeb.repos;
+
+import kg.edu.alatoo.springWeb.modules.Borrower;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Set;
+
+public interface BorrowerRepository extends CrudRepository<Borrower, Long> {
+
+    Set<Borrower> findAll();
+}
